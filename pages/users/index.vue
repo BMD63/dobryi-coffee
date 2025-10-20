228a964921b8ffde8eb35e99b1ce2b058b07e3df
Sergei <chechet@mail.ru>
2025-10-20 15:44:07 +0400

    feat(users): add /users route

---
+++ pages/users/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="h1 mb-4">Пользователи</div>
+
+    <!-- skeleton на время загрузки fetch -->
+    <div v-if="$fetchState.pending">
+      <v-skeleton-loader
+        type="list-item, list-item, list-item, list-item, list-item"
+        class="mb-3"
+      />
+    </div>
+
+    <div v-else>
+      <div class="caption mb-2">Тут позже появится список и фильтры</div>
+      <v-alert type="info" outlined>
+        Заглушка страницы <code>/users</code>. Данные пока не загружаем.
+      </v-alert>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersPage',
+  async fetch () {
+    await new Promise(r => setTimeout(r, 600))
+  },
+  fetchOnServer: false
+}
+</script>
